7c1411425bc8944cc0f96879615c38fa19b98217
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2023-08-14 17:02:27 -0400

    Attempt?

--- 2015/d2/d2.c
+++ 2015/d2/d2.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
 
+int main(void) {
+    FILE *materials_report;
+    int character;
+    int i;
 
-int materials_needed() {
-    FILE* p_materials_form_filled;
-    char buffer[255];
+    if (!(materials_report = fopen("materials_needed.txt", "r"))) {
+        perror("materials_needed.txt");
+        return 1;
+    }
 
-    p_materials_form_filled = fopen("materials_needed.txt", "r");
-    if (p_materials_form_filled == NULL) { printf("Could not open file materials_needed.txt"); return 1; } 
+    int temp, cuboid[3];
+    temp = cuboid[0] = cuboid[1] = cuboid[2] = 0;
+    while ((character = fgetc(materials_report) != EOF)) {
+        switch ((character+'0')) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                // This is the operation that will run for all of the above operators.
+                // Essentially, this will run until we reach an 'x' in which we will dump the temp value into 
+                temp *= 10;
+                temp += character-'0'; // Convert from char to int.
+                break;
+            case 'x':
+            case '\n':
+                // This is the operation that will run for all of the above operators.
+                for (int i = 0; cuboid[i] != 0; i++) { ; }
+                cuboid[i] = temp;
+                temp = 0;
+                break;
+            default:
+                // What did I just read.
+                printf("Unknown character: %d\n", character);
+                break;
+        }
 
-    int lines_in_file = 0;
-    char character;
-    {
-        for (character = getc(p_materials_form_filled); character != EOF; character = getc(p_materials_form_filled)) {
-            printf("character: %c, dvalue: %d\n", character, (int)character);
-            if (character == '\n') { lines_in_file++; }
+        if (character == '\n') {
+            
         }
     }
 
-
-    printf("The file has %d lines\n", character);
-    fclose(p_materials_form_filled);
-    0;
-}
-
-
-int main(void) {
-    char presents_needed[] = "";
-    int status = materials_needed();
-
+    fclose(materials_report);
     return 0;
 }
